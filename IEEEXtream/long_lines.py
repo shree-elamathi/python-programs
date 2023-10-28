97de0161da509b33833b84ac3f7e626c2d5f4a9b
Elamathi <shreeelamathi@gmail.com>
2023-10-28 13:52:06 +0530

    new program added

--- IEEEXtream/long_lines.py
+++ IEEEXtream/long_lines.py
@@ -1,19 +1,25 @@
-def no_of_people_noticeable(n, ho, a, c, q):
-    heights = [ho]
-    noticeable_no= 1
-    stack = [(ho, 1)]
+def is_noticeable(heights, i, j):
+    for k in range(i + 1, j):
+        if heights[k] >= heights[i]:
+            return False
+    return True
+def count_noticeable_people(n, h_0, a, c, q):
+    heights = [h_0]
     for i in range(1, n):
-        height = (stack[-1][0] * a + c) % q
-        while stack and stack[-1][0] <= height:
-            stack.pop()
-        if not stack:
-            noticeable_no = i + 1
-        else:
-            noticeable_no += 1
-        stack.append((height, i + 1))
-    return noticeable_no
+        heights.append((a * heights[i - 1] + c) % q)
 
-n, ho, a, c, q = map(int, input().split())
-result = no_of_people_noticeable(n, ho, a, c, q)
-print(result)
+    noticeable_count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if is_noticeable(heights, i, j):
+                noticeable_count += 1
+    return noticeable_count
+def main():
+    n, h_0, a, c, q = map(int, input().split())
+    noticeable_count = count_noticeable_people(n, h_0, a, c, q)
+    print(noticeable_count)
+
+
+if __name__ == "__main__":
+    main()
 
