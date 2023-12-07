aed169a6c7ec63029c55cd25de2f3b3b98cba081
Elamathi <shreeelamathi@gmail.com>
2023-12-07 23:34:06 +0530

    new program added

--- geeksforgeeks/BST_operations_problem.py
+++ geeksforgeeks/BST_operations_problem.py
@@ -32,17 +32,16 @@ def inorder(root,values=None)->list:
 # the function countPairs(), that returns the count of all pairs of (a, b), where a belongs to one BST
 # and b belongs to another BST, such that a + b = x.
 
-def countpairs(root1,root2,x):
-   arr1=inorder(root1)
-   arr2=inorder(root2)
-   pairs=[]
-   for i in arr1:
-       for j in arr2:
-           if i+j==x:
-               pairs.append(i)
-               pairs.append(j)
-   return (int(len(pairs)/2))
-
+#def countpairs(root1,root2,x):
+#  arr1=inorder(root1)
+#  arr2=inorder(root2)
+#  pairs=[]
+#  for i in arr1:
+#      for j in arr2:
+#          if i+j==x:
+#              pairs.append(i)
+#              pairs.append(j)
+#  return (int(len(pairs)/2))
 
 def leafnodes(root: Node,leafnode=None)->list:
     if leafnode is None:
@@ -57,6 +56,27 @@ def leafnodes(root: Node,leafnode=None)->list:
     if root.right:
         leafnodes(root.right,leafnode)
     return leafnode
+def countpairs(root1,root2,x):
+    arr1=inorder(root1)
+    n1=len(arr1)
+    arr2=inorder(root2)
+    n2=len(arr2)
+    i=0
+    j=-1
+    res=0
+    while i<n1 and j>=-n2:
+        if arr1[i]+arr2[j]==x:
+            res+=1
+            i+=1
+            j-=1
+        elif arr1[i]+arr2[j]<x:
+            i+=1
+        else:
+            j-=1
+    return res
+
+
+
 def isDeadEnd( root):
     arr1=leafnodes(root)
     arr2=inorder(root)
@@ -74,21 +94,38 @@ def isDeadEnd( root):
         return False
 
 #This method is more time complex
-def inorder(root):
-    if root:
-        yield from inorder_generator(root.left)
-        yield root.val
-        yield from inorder_generator(root.right)
-
-def count(root1,root2,x):
-    arr=[]
-    for i in inorder(root1):
-        for j in inorder(root2):
-            if i+j==x:
-                arr.append(i)
-                arr.append(j)
-    return (int(len(arr)/2))
-
+#def inorder(root):
+#   if root:
+#       yield from inorder_generator(root.left)
+#       yield root.val
+#       yield from inorder_generator(root.right)
+#def count(root1,root2,x):
+#   arr=[]
+#   for i in inorder(root1):
+#       for j in inorder(root2):
+#           if i+j==x:
+#               arr.append(i)
+#               arr.append(j)
+#   return (int(len(arr)/2))
+#Another method
+#class Solution:
+#   def dfs(self, root, s):
+#       if root == None: return
+#       s.add(root.data)
+#       self.dfs(root.left, s)
+#       if root.data < x: self.dfs(root.right, s)
+#   def dfs2(self, root, s, x, ans):
+#       if root == None: return
+#       if x - root.data in s:
+#           ans[0] += 1
+#       self.dfs2(root.left, s, x, ans)
+#       if root.data < x: self.dfs2(root.right, s, x, ans)
+#   def countPairs(self, root1, root2, x):
+#       s = set()
+#       self.dfs(root1, s)
+#       ans = [0]
+#       self.dfs2(root2, s, x, ans)
+#       return ans[0]
 
 
 if __name__=='__main__':
