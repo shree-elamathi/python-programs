0abb94f68b044899d6ab2ce99ed938b784dda9c8
Elamathi <shreeelamathi@gmail.com>
2023-12-10 11:13:41 +0530

    new program added

--- geeksforgeeks/Smith_Number.py
+++ geeksforgeeks/Smith_Number.py
@@ -1,3 +1,5 @@
+#Given a number n, the task is to find out whether this number is a Smith number or not.
+# A Smith number is a composite number whose sum of digits is equal to the sum of digits of its prime factors.
 class solution:
     def smithnum(self,n):
         a=str(n)
