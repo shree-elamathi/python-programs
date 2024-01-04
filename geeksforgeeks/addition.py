e3bedd0751ca78c92534e43ac0ab36be400c8554
Elamathi <shreeelamathi@gmail.com>
2024-01-04 19:51:04 +0530

    new program added

--- geeksforgeeks/addition.py
+++ geeksforgeeks/addition.py
@@ -1,5 +1,10 @@
 #addition of two numbers
-num1=eval(input("Enter the 1st value: "))
-num2=eval(input("Enter the 2nd value: "))
-print("The addition of two numbers is: ",num1+num2)
-print("the divided values is",num1//num2)
+#num1=eval(input("Enter the 1st value: "))
+#num2=eval(input("Enter the 2nd value: "))
+#print("The addition of two numbers is: ",num1+num2)
+#print("the divided values is",num1//num2)
+a=10
+b=a
+b=b+1
+print(a)
+print(b)
