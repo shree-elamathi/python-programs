c7993a8d830cb20ffd6fa576c00858484db4e1b7
Elamathi <shreeelamathi@gmail.com>
2024-02-01 20:34:24 +0530

    new program added

--- geeksforgeeks/rev_1st_kelem_in_q.py
+++ geeksforgeeks/rev_1st_kelem_in_q.py
@@ -1,3 +1,11 @@
+#Given an integer K and a queue of integers, we need to reverse the order of the first K elements of the
+# queue, leaving the other elements in the same relative order.Only following standard operations are allowed on queue.
+# enqueue(x) : Add an item x to rear of queue
+# dequeue() : Remove an item from front of queue
+# size() : Returns number of elements in queue.
+# front() : Finds front item.
+# Note: The above operations represent the general processings. In-built functions of the respective
+# languages can be used to solve the problem.
 from collections import deque
 class solution:
     def modifyQueue(self,q,k):
