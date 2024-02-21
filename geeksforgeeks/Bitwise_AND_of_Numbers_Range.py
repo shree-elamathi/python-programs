c812ba0609bdd0c3286be1993c0e617afd46982f
Elamathi <shreeelamathi@gmail.com>
2024-02-21 20:08:39 +0530

    new program added

---
+++ geeksforgeeks/Bitwise_AND_of_Numbers_Range.py
@@ -0,0 +1,17 @@
+# Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers
+# in this range, inclusive.
+class solution:
+    def rangebitwiseand(self, left, right):
+        val=left
+        for i in range(left+1,right+1):
+                val=val&i
+        return val
+#another method
+#time complexity solved
+#while right>left:
+#right = right&(right-1)
+#return right&left
+
+left=5
+right=7
+print(solution().rangebitwiseand(left,right))
