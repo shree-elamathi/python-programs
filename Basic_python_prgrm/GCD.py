ae24e8b19709b8c119e9392a9de9346459451eba
Elamathi <shreeelamathi@gmail.com>
2025-01-08 22:20:16 +0530

    new program added

--- Basic_python_prgrm/GCD.py
+++ Basic_python_prgrm/GCD.py
@@ -1,10 +1,21 @@
 def GCD(m,n):
-    if m<n:
-        m,n=n,m
-    if n==0:
-        return m
+    #base case to have remainder as 1
+    rem = 1
+    if m>n:
+        # the largest val will be dividend and the other will be divisor
+        divd = m
+        divs = n
     else:
-        return GCD(n,m%n)
+        divd = n
+        divs = m
+    # the loop is executed till the remainder becomes 0
+    while rem !=0:
+        rem = divd % divs
+        #even after this if the rem is not 0 then replace the divd and divs
+        if rem != 0:
+            divd = divs
+            divs = rem
+    return divs
 
 x=eval(input("Enter the 1st number: "))
 y=eval(input("Enter the 2nd number: "))
